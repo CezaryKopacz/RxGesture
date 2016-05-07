8da9ecd92d9e8007c1f3da282a53ad44aeacad54
Cezary Kopacz <salvation.sv@gmail.com>
2016-05-07 12:49:25 +0200

    Added Carthage support

---
+++ RxGesture/RxGesture-OSX/RxGesture-OSX.h
@@ -0,0 +1,19 @@
+//
+//  RxGesture-OSX.h
+//  RxGesture-OSX
+//
+//  Created by Cezary Kopacz on 07.05.2016.
+//  Copyright © 2016 MarinTodorov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxGesture-OSX.
+FOUNDATION_EXPORT double RxGesture_OSXVersionNumber;
+
+//! Project version string for RxGesture-OSX.
+FOUNDATION_EXPORT const unsigned char RxGesture_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxGesture_OSX/PublicHeader.h>
+
+
